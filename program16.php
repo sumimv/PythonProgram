8287520b83905f71c0282238d0da827226dbaccc
sumimv <sumimv2006@gmail.com>
2026-02-24 11:36:12 +0530

    Add basic HTML structure with copyright info

---
+++ program16.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML> 
+<html> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf
+8"> 
+<title>Copyright Information</title> 
+</head> 
+<body> 
+<p>&copy <?php echo date('Y'); ?> PHP Exercises - w3resource</p> 
+</body> 
+</html>
