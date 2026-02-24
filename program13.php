b9344c2cbf72ae0dd051782e4c27b0b90f8b437a
sumimv <sumimv2006@gmail.com>
2026-02-24 11:29:42 +0530

    Create program13.php

---
+++ program13.php
@@ -0,0 +1,17 @@
+<?php 
+   function check_vote()  
+   { 
+   $name = "Rakesh"; 
+   $age = 19; 
+   if ($age >= 18)  
+   { 
+     echo $name . ", you Are Eligible For Vote"; 
+   } 
+   else  
+   { 
+     echo $name . ", you are not eligible for vote. "; 
+    } 
+   } 
+   check_vote();  
+ 
+   ?>
